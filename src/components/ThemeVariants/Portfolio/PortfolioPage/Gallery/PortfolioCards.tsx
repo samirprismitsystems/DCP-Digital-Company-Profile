03256851842a4bc358aedf71519ee8a6d99be53b
jaydip <87083314+James-Xitij@users.noreply.github.com>
2023-08-25 10:02:53 +0530

    remov one div from portfolio gallery

--- src/components/ThemeVariants/Portfolio/PortfolioPage/Gallery/PortfolioCards.tsx
+++ src/components/ThemeVariants/Portfolio/PortfolioPage/Gallery/PortfolioCards.tsx
@@ -3,16 +3,14 @@ import { UPLOAD_IMAGE_URI } from "@/services/config";
 export default function PortfolioCards({ path, id }: any) {
   return (
     <>
-      <div className="xs:w-full h-[300px] xlOne:w-[309px] mr-auto mb-6">
-        <div className="xs:min-h-full h-full xlOne:min-h-[3.4rem] mb-5 px-2  py-0 ">
-          <img
-            alt="gallery-img"
-            title="gallery-img"
-            id="upload-image"
-            className="w-full max-w-full h-full block object-cover object-center align-middle"
-            src={`${UPLOAD_IMAGE_URI}/${id}/portfolio/${path}`}
-          />
-        </div>
+      <div className="xs:w-full xlOne:w-[309px] mr-auto xs:min-h-full h-full xlOne:min-h-[3.4rem] mb-5 px-2  py-0 ">
+        <img
+          alt="gallery-img"
+          title="gallery-img"
+          id="upload-image"
+          className="w-full max-w-full h-full block object-cover object-center align-middle"
+          src={`${UPLOAD_IMAGE_URI}/${id}/portfolio/${path}`}
+        />
       </div>
     </>
   );
