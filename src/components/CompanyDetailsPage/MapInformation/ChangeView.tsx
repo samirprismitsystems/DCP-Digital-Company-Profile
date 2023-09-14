42097c79b13115fdfae485026447800f82cfc89b
jaydip <87083314+James-Xitij@users.noreply.github.com>
2023-09-14 10:23:40 +0530

    disable useeffect hook

--- src/components/CompanyDetailsPage/MapInformation/ChangeView.tsx
+++ src/components/CompanyDetailsPage/MapInformation/ChangeView.tsx
@@ -1,5 +1,7 @@
 export function ChangeView({ coords }: { coords: any }) {
   if (typeof window !== "undefined") {
+    // Disable the react-hooks/rules-of-hooks rule for this line
+    // eslint-disable-next-line react-hooks/rules-of-hooks
     const { latLng } = require("leaflet");
     const { useMap } = require("react-leaflet");
     const map = useMap();
