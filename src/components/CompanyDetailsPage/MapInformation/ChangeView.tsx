82abb826b9bd17ce08a7d15ca467ecea499c2fab
jaydip <87083314+James-Xitij@users.noreply.github.com>
2023-09-14 10:29:16 +0530

    add placeholder

--- src/components/CompanyDetailsPage/MapInformation/ChangeView.tsx
+++ src/components/CompanyDetailsPage/MapInformation/ChangeView.tsx
@@ -1,10 +1,10 @@
 export function ChangeView({ coords }: { coords: any }) {
   if (typeof window !== "undefined") {
-    // Disable the react-hooks/rules-of-hooks rule for this line
-    // eslint-disable-next-line react-hooks/rules-of-hooks
     const { latLng } = require("leaflet");
     const { useMap } = require("react-leaflet");
+    // eslint-disable-next-line react-hooks/rules-of-hooks
     const map = useMap();
+
     const center = latLng(coords);
     map.setView(center, 17);
   }
