22e75d1ace2067b5bcc38bb9e955158e19f36456
jaydip <87083314+James-Xitij@users.noreply.github.com>
2023-09-19 18:05:56 +0530

    top to bottom icon size issues

--- src/common/BottopToTop.tsx
+++ src/common/BottopToTop.tsx
@@ -41,7 +41,7 @@ const BottomToTop = () => {
           className="fixed bottom-4 text-[3rem] right-4 bg-primary-main opacity-50 hover:opacity-100 px-6 py-2 rounded-md"
           onClick={handleScrollToTop}
         >
-          <FontAwesomeIcon className="text-black" icon={faCaretUp} />
+          <FontAwesomeIcon className=" w-[30px] h-[25px] text-black" icon={faCaretUp} />
         </button>
       )}
     </>
