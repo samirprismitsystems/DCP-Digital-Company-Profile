a46aecc936880429001cb9784b23c885170d6fa7
jaydip <87083314+James-Xitij@users.noreply.github.com>
2023-09-19 11:18:58 +0530

    resolve the bugs for the payment option

--- src/components/PaymentOptionPage/PaymentOptionPage.tsx
+++ src/components/PaymentOptionPage/PaymentOptionPage.tsx
@@ -130,13 +130,13 @@ export default function PaymentOptionPage() {
       </div>
       <FormProvider {...objForm}>
         <form
-          className="mb-16 elative digital_profile_form form_shadow bg-white rounded-2xl p-10 pb-0 block"
+          className="mb-16 elative digital_profile_form form_shadow bg-white rounded-2xl p-10 pb-0 block relative_box_for_loading"
           style={{
             boxShadow: "0rem 0rem 1rem 0px rgb(28 66 77 / 15%)",
           }}
           onSubmit={objForm.handleSubmit(onSave)}
         >
-          {isLoading ? (
+          {true ? (
             <div className="py-[10rem]">
               <Loading />
             </div>
