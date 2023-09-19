9db7cb6c623b798281858c51a6a33cee9c1887a6
jaydip <87083314+James-Xitij@users.noreply.github.com>
2023-09-19 18:08:55 +0530

    add height for the button

--- src/common/BottopToTop.tsx
+++ src/common/BottopToTop.tsx
@@ -38,10 +38,10 @@ const BottomToTop = () => {
     <>
       {showButton && (
         <button
-          className="fixed bottom-4 text-[3rem] right-4 bg-primary-main opacity-50 hover:opacity-100 px-6 py-2 rounded-md"
+          className="h-[60px] fixed bottom-4 text-[3rem] right-4 bg-primary-main opacity-50 hover:opacity-100 px-6 py-2 rounded-md"
           onClick={handleScrollToTop}
         >
-          <FontAwesomeIcon className=" w-[30px] h-[25px] text-black" icon={faCaretUp} />
+          <FontAwesomeIcon className=" w-[30px] h-[30px] text-black" icon={faCaretUp} />
         </button>
       )}
     </>
