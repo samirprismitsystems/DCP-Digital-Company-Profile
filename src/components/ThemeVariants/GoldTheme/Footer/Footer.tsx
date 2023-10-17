b2e83b9544627cf450263b60140bb1fe84df6678
samir <samir.s@prismitsystems.com>
2023-10-17 13:06:51 +0530

    change footer code

--- src/components/ThemeVariants/GoldTheme/Footer/Footer.tsx
+++ src/components/ThemeVariants/GoldTheme/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <section className='bg-gray-200 text-center xs:px-[7rem] md:px-4 text-black py-14 px-4'>
-      <span className='text-[1.8rem]'>All Rights Reserved. &copy; 2015 </span>
+      <span className='text-[1.8rem]'>&copy; Copyright 2009 - 2023 All Rights Reserved </span>
       <span className='inline-block py-4'><Link className='text-[1.8rem] xs:py-4 md:py-0 border-b border-b-blue-600 border-dotted text-blue-600' href="https://www.prismitsystems.com/" target='_blank'>Prism IT Systems</Link></span>
     </section>
   )
