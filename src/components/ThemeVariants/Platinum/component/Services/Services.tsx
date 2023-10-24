67cbcc7756aca39457babb87b1c76bc2b21d797c
samir <samir.s@prismitsystems.com>
2023-10-24 12:06:37 +0530

    create service section

--- src/components/ThemeVariants/Platinum/component/Services/Services.tsx
+++ src/components/ThemeVariants/Platinum/component/Services/Services.tsx
@@ -1,109 +1,39 @@
-import React from 'react'
+import { ThemeContextApi } from "@/pages/[slug]";
+import Utils from "@/services/Utils";
+import { UPLOAD_IMAGE_URI } from "@/services/config";
+import { useContext } from "react";
+import styles from "./styles/services.module.scss";
+
 
 export default function Services() {
-  return (
-      <section id="services" className="services_area pt-platinum120 pb-platinum120">
-          <div className="container">
-              <div className="row justify-center">
-                  <div className="w-full platinumLg:w-1/2">
-                      <div className="section_title text-center pb-platinum6">
-                          <h5 className="sub_title">What We Do</h5>
-                          <h4 className="main_title">Our Services</h4>
-                      </div> 
-                  </div>
-              </div> 
-              <div className="row justify-center">
-                  <div className="w-full platinumSm:w-10/12 platinumMd:w-6/12 platinumLg:w-4/12">
-                      <div className="single_services text-center mt-platinum8 mx-platinum3">
-                          <div className="services_icon">
-                              <i className="lni lni-layout"></i>
-                              <svg xmlns="http://www.w3.org/2000/svg" width="94" height="92" viewBox="0 0 94 92">
-                                  <path className="services_shape" id="Polygon_12" data-name="Polygon 12" d="M42.212,2.315a11,11,0,0,1,9.576,0l28.138,13.6a11,11,0,0,1,5.938,7.465L92.83,54.018A11,11,0,0,1,90.717,63.3L71.22,87.842A11,11,0,0,1,62.607,92H31.393a11,11,0,0,1-8.613-4.158L3.283,63.3A11,11,0,0,1,1.17,54.018L8.136,23.383a11,11,0,0,1,5.938-7.465Z" />
-                              </svg>
-                          </div>
-                          <div className="services_content mt-platinum5 platinumXl:mt-platinum10">
-                              <h3 className="services_title text-black font-semibold text-xl platinumMd:text-2xl platinumLg:text-xl platinumXl:text-3xl">Web Design</h3>
-                              <p className="mt-platinum4">Lorem ipsum dolor sit amet, consetetur sadi aliquyam erat, sed diam voluptua. At vero eos accusam et justo duo dolores </p>
-                          </div>
-                      </div> 
-                  </div>
-
-                  <div className="w-full platinumSm:w-10/12 platinumMd:w-6/12 platinumLg:w-4/12">
-                      <div className="single_services text-center mt-platinum8 mx-platinum3">
-                          <div className="services_icon">
-                              <i className="lni lni-bullhorn"></i>
-                              <svg xmlns="http://www.w3.org/2000/svg" width="94" height="92" viewBox="0 0 94 92">
-                                  <path className="services_shape" id="Polygon_12" data-name="Polygon 12" d="M42.212,2.315a11,11,0,0,1,9.576,0l28.138,13.6a11,11,0,0,1,5.938,7.465L92.83,54.018A11,11,0,0,1,90.717,63.3L71.22,87.842A11,11,0,0,1,62.607,92H31.393a11,11,0,0,1-8.613-4.158L3.283,63.3A11,11,0,0,1,1.17,54.018L8.136,23.383a11,11,0,0,1,5.938-7.465Z" />
-                              </svg>
-                          </div>
-                          <div className="services_content mt-platinum5 platinumXl:mt-platinum10">
-                              <h3 className="services_title text-black font-semibold text-xl platinumMd:text-2xl platinumLg:text-xl platinumXl:text-3xl">Digital Marketing</h3>
-                              <p className="mt-platinum4">Lorem ipsum dolor sit amet, consetetur sadi aliquyam erat, sed diam voluptua. At vero eos accusam et justo duo dolores </p>
-                          </div>
-                      </div> 
-                  </div>
-
-                  <div className="w-full platinumSm:w-10/12 platinumMd:w-6/12 platinumLg:w-4/12">
-                      <div className="single_services text-center mt-platinum8 mx-platinum3">
-                          <div className="services_icon">
-                              <i className="lni lni-mobile"></i>
-                              <svg xmlns="http://www.w3.org/2000/svg" width="94" height="92" viewBox="0 0 94 92">
-                                  <path className="services_shape" id="Polygon_12" data-name="Polygon 12" d="M42.212,2.315a11,11,0,0,1,9.576,0l28.138,13.6a11,11,0,0,1,5.938,7.465L92.83,54.018A11,11,0,0,1,90.717,63.3L71.22,87.842A11,11,0,0,1,62.607,92H31.393a11,11,0,0,1-8.613-4.158L3.283,63.3A11,11,0,0,1,1.17,54.018L8.136,23.383a11,11,0,0,1,5.938-7.465Z" />
-                              </svg>
-                          </div>
-                          <div className="services_content mt-platinum5 platinumXl:mt-platinum10">
-                              <h3 className="services_title text-black font-semibold text-xl platinumMd:text-2xl platinumLg:text-xl platinumXl:text-3xl">Mobile Apps</h3>
-                              <p className="mt-platinum4">Lorem ipsum dolor sit amet, consetetur sadi aliquyam erat, sed diam voluptua. At vero eos accusam et justo duo dolores </p>
-                          </div>
-                      </div> 
-                  </div>
-
-                  <div className="w-full platinumSm:w-10/12 platinumMd:w-6/12 platinumLg:w-4/12">
-                      <div className="single_services text-center mt-platinum8 mx-platinum3">
-                          <div className="services_icon">
-                              <i className="lni lni-seo"></i>
-                              <svg xmlns="http://www.w3.org/2000/svg" width="94" height="92" viewBox="0 0 94 92">
-                                  <path className="services_shape" id="Polygon_12" data-name="Polygon 12" d="M42.212,2.315a11,11,0,0,1,9.576,0l28.138,13.6a11,11,0,0,1,5.938,7.465L92.83,54.018A11,11,0,0,1,90.717,63.3L71.22,87.842A11,11,0,0,1,62.607,92H31.393a11,11,0,0,1-8.613-4.158L3.283,63.3A11,11,0,0,1,1.17,54.018L8.136,23.383a11,11,0,0,1,5.938-7.465Z" />
-                              </svg>
-                          </div>
-                          <div className="services_content mt-platinum5 platinumXl:mt-platinum10">
-                              <h3 className="services_title text-black font-semibold text-xl platinumMd:text-2xl platinumLg:text-xl platinumXl:text-3xl">SEO Consultancy</h3>
-                              <p className="mt-platinum4">Lorem ipsum dolor sit amet, consetetur sadi aliquyam erat, sed diam voluptua. At vero eos accusam et justo duo dolores </p>
-                          </div>
-                      </div> 
-                  </div>
-
-                  <div className="w-full platinumSm:w-10/12 platinumMd:w-6/12 platinumLg:w-4/12">
-                      <div className="single_services text-center mt-platinum8 mx-platinum3">
-                          <div className="services_icon">
-                              <i className="lni lni-layers"></i>
-                              <svg xmlns="http://www.w3.org/2000/svg" width="94" height="92" viewBox="0 0 94 92">
-                                  <path className="services_shape" id="Polygon_12" data-name="Polygon 12" d="M42.212,2.315a11,11,0,0,1,9.576,0l28.138,13.6a11,11,0,0,1,5.938,7.465L92.83,54.018A11,11,0,0,1,90.717,63.3L71.22,87.842A11,11,0,0,1,62.607,92H31.393a11,11,0,0,1-8.613-4.158L3.283,63.3A11,11,0,0,1,1.17,54.018L8.136,23.383a11,11,0,0,1,5.938-7.465Z" />
-                              </svg>
-                          </div>
-                          <div className="services_content mt-platinum5 platinumXl:mt-platinum10">
-                              <h3 className="services_title text-black font-semibold text-xl platinumMd:text-2xl platinumLg:text-xl platinumXl:text-3xl">Graphic Design</h3>
-                              <p className="mt-platinum4">Lorem ipsum dolor sit amet, consetetur sadi aliquyam erat, sed diam voluptua. At vero eos accusam et justo duo dolores </p>
-                          </div>
-                      </div> 
-                  </div>
-
-                  <div className="w-full platinumSm:w-10/12 platinumMd:w-6/12 platinumLg:w-4/12">
-                      <div className="single_services text-center mt-platinum8 mx-platinum3">
-                          <div className="services_icon">
-                              <i className="lni lni-briefcase"></i>
-                              <svg xmlns="http://www.w3.org/2000/svg" width="94" height="92" viewBox="0 0 94 92">
-                                  <path className="services_shape" id="Polygon_12" data-name="Polygon 12" d="M42.212,2.315a11,11,0,0,1,9.576,0l28.138,13.6a11,11,0,0,1,5.938,7.465L92.83,54.018A11,11,0,0,1,90.717,63.3L71.22,87.842A11,11,0,0,1,62.607,92H31.393a11,11,0,0,1-8.613-4.158L3.283,63.3A11,11,0,0,1,1.17,54.018L8.136,23.383a11,11,0,0,1,5.938-7.465Z" />
-                              </svg>
-                          </div>
-                          <div className="services_content mt-platinum5 platinumXl:mt-platinum10">
-                              <h3 className="services_title text-black font-semibold text-xl platinumMd:text-2xl platinumLg:text-xl platinumXl:text-3xl">Business Consultancy</h3>
-                              <p className="mt-platinum4">Lorem ipsum dolor sit amet, consetetur sadi aliquyam erat, sed diam voluptua. At vero eos accusam et justo duo dolores </p>
-                          </div>
-                      </div> 
-                  </div>
-              </div> 
-          </div> 
-      </section>
-  )
+    const objItem = useContext(ThemeContextApi).service;
+    return (
+        <section id="services" className="bg-white services_area pt-platinum120 pb-platinum120">
+            <div className="container">
+                <div className="flex items-center justify-center">
+                    <div className="w-full">
+                        <div className={`${styles.section_title} text-center pb-platinum6`}>
+                            <h5 className={styles.sub_title}>What We Do</h5>
+                            <h4 className={styles.main_title}>Our Services</h4>
+                        </div>
+                    </div>
+                </div>
+                <div className="grid grid-cols-3 gap-8">
+                    {objItem && objItem.map((item) => (
+                        <div className="w-full platinumSm:w-10/12 platinumMd:w-6/12 platinumLg:w-full">
+                            <div className={`${styles.single_services} text-center mt-platinum8 mx-platinum3`}>
+                                <div className={`${styles.services_icon} flex items-center justify-center`}>
+                                    <img src={`${UPLOAD_IMAGE_URI}/${item.company_id || Utils.getCompanyID()}/service/${item.service_image}`} className={styles.services_shape} alt="" />
+                                </div>
+                                <div className="services_content mt-platinum5 platinumXl:mt-platinum10">
+                                    <h3 className="services_title text-black font-semibold text-xl platinumMd:text-2xl platinumLg:text-xl platinumXl:text-3xl">{Utils.getContent(item.service_name)}</h3>
+                                    <p className="mt-platinum4">{Utils.getContent(item.service_desc)}</p>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+    )
 }
